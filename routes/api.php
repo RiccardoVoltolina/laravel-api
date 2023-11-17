a2ea096b6491a611ce42dbcaa03bbfbb02ab176c
RiccardoVoltolina <riky.volto@live.com>
2023-11-17 16:30:49 +0100

    create project controller function

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('projects', function(){
-    return [
-        'status' => 'succes',
-        'result' => Project::all(),
-    ];
-});
+// Route::get('projects', function(){
+//     return [
+//         'status' => 'succes',
+//         'result' => Project::all(),
+//     ];
+// });
+
+Route::get('projects', [ProjectController::class, 'index']);
+
