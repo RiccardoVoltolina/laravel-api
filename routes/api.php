beb44f4dc75d3edbcf82b9f70525358733bbe09e
RiccardoVoltolina <riky.volto@live.com>
2023-11-21 21:07:26 +0100

    change api pagination

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     ];
 // });
 
+// questo è il percorso da scrivere, per visualizzare l'array lato API, il projects deve essere preceduto da /api
+
 Route::get('projects', [ProjectController::class, 'index']);
 
 // vado a richiamare la funzione show implementata nell' api ProjectController
